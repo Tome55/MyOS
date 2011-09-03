6d97499cd9b3d620c201123ba8641c41aab6dd95
jittiyut karasong <jittiyut47@hotmail.com>
2011-09-03 11:24:07 +0700

    ignore some file

---
+++ MyOS/MyOSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MyOSAppDelegate.h
+//  MyOS
+//
+//  Created by jittiyut on 9/3/54 BE.
+//  Copyright 2554 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyOSAppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
